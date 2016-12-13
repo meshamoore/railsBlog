63963aab579ad742273d51c13f69ac46c1c4b36b
zshifrin <zach.shifrin@gmail.com>
2016-12-13 16:53:21 -0600

    added follow model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts
+
+  has_many :passive_relationships, class_name: 'Follow', foreign_key: 'target_id'
+  has_many :active_relationships, class_name: 'Follow', foreign_key: 'user_id'
+
+  has_many :followers, through: :passive_relationships, source: :follower
+  has_many :following, through: :active_relationships,  source: :target
 end
