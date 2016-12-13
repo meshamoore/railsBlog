0e540e1d72cc6bb9dd03f870edb6ad17b4dfbe0c
Mesha Moore <meshaathome@hotmail.com>
2016-12-13 17:39:44 -0600

    updated controller files

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.content = params[:content]
+    @post.user_id = params[:user_id]
+
+    if params[:title] || params[:content] == ''
+      flash[:alert] = "All fields are required!"
+      redirect_to("/posts/new")
+    else 
+      @post.save
+      flash[:notice] = "Post successfully created!"
+      redirect_to("/posts")
+    end
+  end
+
+  def new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:title]
+    @post.content = params[:content]
+
+    if params[:title].empty? || false == @post.save
+      flash[:alert] = "Failed to update post!"
+      redirect_to("/posts/" + @post.id.to_s + "/edit")
+    else 
+      flash[:notice] = "Post successfully updated!"
+      redirect_to("/posts")
+    end
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    flash[:notice] = "Post successfully deleted!"
+    redirect_to("/posts")
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+# GET /users
+  def index
+    @users = User.all
+  end
+
+  def create
+  	@user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "User successfully created!"
+      redirect_to("/users")
+    else 
+      render("new")
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = "User successfully updated!"
+      redirect_to("/users")
+    else
+      render("edit")
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:notice] = "User successfully deleted!"
+    redirect_to("/users")
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:email)
+  end
+end
