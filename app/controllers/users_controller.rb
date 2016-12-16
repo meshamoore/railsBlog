4d7f1044f186fabe467242a73acc33aecfe9bf8e
Justin Tallant <jtallant07@gmail.com>
2016-12-16 16:04:08 -0600

    add user avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:email, :name, :password)
+      params.require(:user).permit(:email, :name, :password, :avatar)
     end
 end
