37e269124d9795abbd8040b3299b6edb93ba7b8c
Justin Tallant <jtallant07@gmail.com>
2016-12-19 13:35:29 -0600

    methods for follow, unfollow, feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,26 @@ class User < ApplicationRecord
 
   has_many :posts
 
-  has_many :passive_relationships, class_name: 'Follow', foreign_key: 'target_id'
   has_many :active_relationships, class_name: 'Follow', foreign_key: 'user_id'
-
-  has_many :followers, through: :passive_relationships, source: :follower
   has_many :following, through: :active_relationships,  source: :target
 
-  def follow(target)
-    Follow.create(user_id: id, target_id: target.id)
+  has_many :passive_relationships, class_name: 'Follow', foreign_key: 'target_id'
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  def follow(user)
+    active_relationships.create(target_id: user.id)
+  end
+
+  def unfollow(user)
+     active_relationships.find_by(target_id: user.id).destroy
+  end
+
+  def following?(user)
+    following.include?(user)
   end
 
   def feed
-    following_ids = "SELECT target_id FROM follows WHERE  user_id = :user_id"
+    following_ids = "SELECT target_id FROM follows WHERE user_id = :user_id"
     Post.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 end
