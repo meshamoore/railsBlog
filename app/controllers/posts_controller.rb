9bc8bf955cdf9a29ad64ac1f92957e3c02e7b4b5
zshifrin <zach.shifrin@gmail.com>
2016-12-19 16:58:51 -0600

    allowed comments and changed text box size

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :confirm_logged_in
-  before_action :confirm_post_owner, except: [:new, :create]
+  before_action :confirm_post_owner, except: [:new, :create, :show]
 
   def create
     @post = Post.new(post_params)
